81c16d91c56504cb416a81579226c5f8f6dda405
AYHM13x <ayhmalakel1995@gmail.com>
2023-10-01 19:50:38 +0300

    create card books method in cubits

--- lib/Features/home/presentation/view_models/card_books_cubits/card_books_cubit.dart
+++ lib/Features/home/presentation/view_models/card_books_cubits/card_books_cubit.dart
@@ -1,9 +1,30 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/Features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'card_books_state.dart';
 
 class CardBooksCubit extends Cubit<CardBooksState> {
-  CardBooksCubit() : super(CardBooksInitial());
+  CardBooksCubit(this.homeRepo) : super(CardBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchCardBooks() async {
+    emit(CardBooksLoading());
+    var result = await homeRepo.fetchCardBooks();
+
+    result.fold(
+      (failure) {
+        emit(
+          CardBooksFailure(failure.errMessage),
+        );
+      },
+      (listOfBooks) {
+        emit(
+          CardBooksSuccess(listOfBooks),
+        );
+      },
+    );
+  }
 }
